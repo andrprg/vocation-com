be5a73fbeb113b367f983617beb5443853a01221
Andrprg <andrprg@rambler.ru>
2019-07-16 15:48:08 +0300

    Add Func: Количество дней в полных месяцах

--- src/app/Services/vocation.service.ts
+++ src/app/Services/vocation.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Month } from '../Models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VocationService {
 
+  private readonly AVERAGE_DAYS_WORK = 29.3;
+
   constructor() { }
+
+    //Количество дней в полных месяцах
+    getCountDaysInFullMonth(months: Month[]): number {
+      const countMonths = months.filter(value => value.excludeCountDay === 0).length;
+      return this.AVERAGE_DAYS_WORK * countMonths;
+    }
 }
