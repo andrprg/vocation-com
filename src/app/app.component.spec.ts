761bbe4f50369b3131ca76849a166cc1f622de3c
Andrprg <andrprg@rambler.ru>
2019-07-16 16:00:42 +0300

    Очищен app.component.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('vocation-com');
   });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to vocation-com!');
-  });
 });
