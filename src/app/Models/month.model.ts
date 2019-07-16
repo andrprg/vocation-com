5b0e1d5d4239e67470a082507436702b51a54b44
Andrprg <andrprg@rambler.ru>
2019-07-16 14:58:00 +0300

    mponth.model реализован конструктор

--- src/app/Models/month.model.ts
+++ src/app/Models/month.model.ts
@@ -1,10 +1,12 @@
 export default class Month {
     constructor(
         public month: Date,
-        public summ: number,
-        public excludeCountDay: number
+        public summ?: number,
+        public excludeCountDay?: number
 
     ) {
+        this.summ = summ || 0;
+        this.excludeCountDay = excludeCountDay || 0;
     }
 
 }
