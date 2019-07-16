33ed0e06504a11b5f38e7433d4651139d80861f4
Andrprg <andrprg@rambler.ru>
2019-07-16 16:15:34 +0300

    Add Func: Количество дней в не полных месяцах

--- src/app/Services/vocation.service.ts
+++ src/app/Services/vocation.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Month } from '../Models';
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,23 @@ export class VocationService {
 
   constructor() { }
 
-    //Количество дней в полных месяцах
-    getCountDaysInFullMonth(months: Month[]): number {
-      const countMonths = months.filter(value => value.excludeCountDay === 0).length;
-      return this.AVERAGE_DAYS_WORK * countMonths;
-    }
+  //Количество дней в полных месяцах
+  getCountDaysInFullMonth(months: Month[]): number {
+    const countMonths = months.filter(value => value.excludeCountDay === 0).length;
+    return this.AVERAGE_DAYS_WORK * countMonths;
+  }
+
+  //Количество дней в не полных месяцах
+  getCountDaysInNotFullMonth(months: Month[]): number {
+    const countDayInMonths = months
+      .filter(value => value.excludeCountDay > 0)
+      .reduce((acc, value) => {
+        //(29.3 * количество рабочих дней)/количество дней в месяце
+        const rb = moment(value.month).daysInMonth() - value.excludeCountDay;
+        const ret = (this.AVERAGE_DAYS_WORK * rb) / moment(value.month).daysInMonth();
+        return acc += ret;
+      }, 0)
+
+    return countDayInMonths;
+  }
 }
