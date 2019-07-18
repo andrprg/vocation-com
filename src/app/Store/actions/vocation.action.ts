8cfd0c64b4a78cbc64f2351f262bb5a55bff7d49
Andrprg <andrprg@rambler.ru>
2019-07-18 13:06:19 +0300

    Реализован action класс vocation

---
+++ src/app/Store/actions/vocation.action.ts
@@ -0,0 +1,26 @@
+import { Action } from '@ngrx/store';
+import { Month } from '../../Models';
+
+export const LOAD_MONTHS = '[MONTHS] Load Months';
+export const LOAD_MONTHS_SUCCESS = '[MONTHS] Load Months Success';
+export const LOAD_MONTHS_FAIL = '[MONTHS] Load Months Fail';
+
+export class LoadMONTHS implements Action{
+    readonly type = LOAD_MONTHS;
+}
+
+export class LoadMonthsSuccess implements Action{
+    readonly type = LOAD_MONTHS_SUCCESS;
+    constructor(
+        public payload: Month[]
+    ){}
+}
+
+export class LoadMonthsFail implements Action{
+    readonly type = LOAD_MONTHS_FAIL;
+    constructor(
+        public error: any
+    ){}
+}
+
+export type All = LoadMONTHS | LoadMonthsSuccess | LoadMonthsFail;
