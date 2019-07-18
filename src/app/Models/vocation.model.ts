e45c046f343b1207e7d85f7baf886fea9479ed36
Andrprg <andrprg@rambler.ru>
2019-07-18 12:09:14 +0300

    Убрано лишнее поле

--- src/app/Models/vocation.model.ts
+++ src/app/Models/vocation.model.ts
@@ -2,13 +2,11 @@ import Month from './month.model';
 export default class Vocation {
     constructor(
         public dateFrom: Date,
-        public month?: Month[],
         public countDay?: number,
         public dateFromWork?: Date
 
     ) {
         this.countDay = countDay || 0;
-        this.month = month || [];
     }
 
 }
