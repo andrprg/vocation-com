44e7d384ae88f133f212fbdd951e21159c1c69cc
Andrprg <andrprg@rambler.ru>
2019-07-22 14:37:10 +0300

    Add components and add store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { reducers } from './Store';
+import { MonthEffect } from './Store/effects/month.effects';
+import { VocationContainerComponent } from './Components/vocation-container/vocation-container.component';
+import { VocationComponent } from './Components/vocation/vocation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VocationContainerComponent,
+    VocationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([MonthEffect]),
   ],
   providers: [],
   bootstrap: [AppComponent]
