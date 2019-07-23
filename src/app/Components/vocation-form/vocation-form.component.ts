a6d6d88172d509fc276d09ccfcab0cd7fcea77d3
Andrprg <andrprg@rambler.ru>
2019-07-23 10:20:18 +0300

    Добавлена функция  добавления валидатора к  datafromWork

--- src/app/Components/vocation-form/vocation-form.component.ts
+++ src/app/Components/vocation-form/vocation-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy, Output, EventEmitter  } from '@angular/core';
 import { Vocation } from 'src/app/Models';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators, ValidatorFn } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -13,6 +13,7 @@ export class VocationFormComponent implements OnInit, OnDestroy{
   @Output() onSubmit = new EventEmitter<Vocation>();
   formGroup: FormGroup;
   private sub: Subscription;
+  isFromWork:boolean = false;
 
   constructor(private fb: FormBuilder) { }
 
@@ -24,7 +25,6 @@ export class VocationFormComponent implements OnInit, OnDestroy{
     this.formGroup = this.fb.group({
       dateFrom: new FormControl(null, [Validators.required]),
       dateFromWork: new FormControl(null),
-      isFromWork: new FormControl(false),
       countDay: new FormControl(28, [
         Validators.required,
         Validators.pattern(/^[1-9]*$/)
@@ -34,6 +34,20 @@ export class VocationFormComponent implements OnInit, OnDestroy{
 
   ngOnDestroy() {
     if (this.sub) this.sub.unsubscribe;
+  }
+
+  toggleDateFromWorkValidator(){
+    this.isFromWork = !this.isFromWork;
+    const dateWork = this.formGroup.get('dateFromWork');
+    const dateFromWorkValidators: ValidatorFn[] = [
+      Validators.required
+    ];   
+    if (this.isFromWork) {
+      dateWork.setValidators(dateFromWorkValidators);
+    } else {
+      dateWork.clearValidators();
+    }
+    dateWork.updateValueAndValidity();     
   }
 
 }
