5e2aeb9ddf71a12081544515f4cdb66c25e15840
Andrprg <andrprg@rambler.ru>
2019-07-24 10:56:32 +0300

    Реализован pipe

--- src/app/Pipes/date-to-name.pipe.ts
+++ src/app/Pipes/date-to-name.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
 
 @Pipe({
   name: 'dateToName'
@@ -6,7 +7,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DateToNamePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return null;
+    return moment(value).format('MMMM YYYY').toUpperCase();
   }
 
 }
