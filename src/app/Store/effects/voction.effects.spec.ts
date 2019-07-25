d2cd6addc93e30121b0b5856d1a682c976c7a349
Andrprg <andrprg@rambler.ru>
2019-07-25 16:42:42 +0300

    Change test month-effect

--- src/app/Store/effects/voction.effects.spec.ts
+++ src/app/Store/effects/voction.effects.spec.ts
@@ -1,55 +1,29 @@
 import { TestBed } from "@angular/core/testing";
-import { provideMockActions } from '@ngrx/effects/testing';
-import { cold, hot } from 'jasmine-marbles';
-import { MonthEffect } from './month.effects';
-import * as Actions from '../actions/month.action';
-import { Observable } from 'rxjs';
-import { VocationService } from 'src/app/Services/vocation.service';
+import { provideMockActions } from "@ngrx/effects/testing";
+import { ReplaySubject } from "rxjs";
 
-import { Vocation, Month } from 'src/app/Models';
+import { MonthEffect } from "./month.effects";
+import * as MyActions from  "../actions/month.action";
 
-describe('Testing Effects', () => {
-    let monthEffect:MonthEffect;
-    let actions$: Observable<any>;
-    let monthService;
-    let months = [
-        new Month(new Date(2019, 1), 100, 7),
-        new Month(new Date(2019, 2), 100, 0),
-        new Month(new Date(2019, 3), 100, 0),
-        new Month(new Date(2019, 4), 100, 0),
-        new Month(new Date(2019, 5), 173, 3)
-      ];
-  
-      let vocation = new Vocation(new Date(2019,7, 1),28);
+describe("Testing Month Effects", () => {
+  let monthEffect: MonthEffect;
+  let actions: ReplaySubject<any>;
 
-    beforeEach(() => TestBed.configureTestingModule({
-        imports: [
-        ],
-        providers: [
-          MonthEffect,
-          /*
-          provideMockActions(() => actions$),
-          {
-            provide: VocationService,
-            useValue: jasmine.createSpyObj('vocationService', ['getMonths'])
-          }
-          */
-         ]
-      }));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [],
+      providers: [MonthEffect, provideMockActions(() => actions)]
+    });
+    monthEffect = TestBed.get(MonthEffect);
+  });
 
-/*
+  it("должен работать (LOAD_MONTHS_SUCCESS)", () => {
+    actions = new ReplaySubject(1);
+    actions.next(MyActions.LOAD_MONTHS);
 
-      monthEffect = TestBed.get(MonthEffect);
-      monthService = TestBed.get(monthService);
+    monthEffect.loadMonths$.subscribe(result => {
+      expect(result).toEqual(MyActions.LOAD_MONTHS_SUCCESS);
+    });
+  });
 
-      it('should work', () => {
-        const action = new Actions.LoadMonths(vocation);
-        const completion = new Actions.LoadMonthsSuccess(months);
-     
-        actions$ = hot('--a-', { a: action });
-        const expected = cold('--b', { b: completion });
-     
-        expect(monthEffect.loadMonths$).toBeObservable(expected);
-      });
-      */
-})
+});
